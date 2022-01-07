20a5afe2a688a481bd125941c8d39c31a2e61cea
Daniel Tyebkhan <dtyebkhan@gmail.com>
2022-01-07 07:37:38 -0800

    removed and gate entity

--- src/main/java/com/bitwisemc/org/core/AndGateBlock.java
+++ src/main/java/com/bitwisemc/org/core/AndGateBlock.java
@@ -1,18 +1,12 @@
 package com.bitwisemc.org.core;
 
-import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.DiodeBlock;
-import net.minecraft.world.level.block.EntityBlock;
-import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
-import org.jetbrains.annotations.Nullable;
 
-import static net.minecraft.world.level.block.piston.MovingPistonBlock.FACING;
-
-public class AndGateBlock extends DiodeBlock implements EntityBlock {
+public class AndGateBlock extends DiodeBlock {
     public AndGateBlock(Properties props) {
         super(props);
         this.registerDefaultState(
@@ -27,17 +21,8 @@ public class AndGateBlock extends DiodeBlock implements EntityBlock {
         return 0;
     }
 
-    @Nullable
-    @Override
-    public BlockEntity newBlockEntity(BlockPos pPos, BlockState pState) {
-//        return new AndGateBlockEntity(pPos, pState);
-        return null;
-    }
-
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
         pBuilder.add(FACING, POWERED);
     }
-
-
 }
