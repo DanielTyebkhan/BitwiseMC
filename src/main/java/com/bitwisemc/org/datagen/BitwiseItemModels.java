c006de4ff5cc953522b32a912afdecfb485f3481
Daniel Tyebkhan <dtyebkhan@gmail.com>
2022-01-08 12:31:03 -0800

    Fixed item models

--- src/main/java/com/bitwisemc/org/datagen/BitwiseItemModels.java
+++ src/main/java/com/bitwisemc/org/datagen/BitwiseItemModels.java
@@ -3,6 +3,8 @@ package com.bitwisemc.org.datagen;
 import com.bitwisemc.org.BitwiseMC;
 import com.bitwisemc.org.setup.Registration;
 import net.minecraft.data.DataGenerator;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.client.model.ModelLoaderRegistry;
 import net.minecraftforge.client.model.generators.ItemModelProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
 
@@ -12,10 +14,21 @@ public class BitwiseItemModels extends ItemModelProvider {
         super(generator, BitwiseMC.MODID, helper);
     }
 
+    private void registerGateModel(String name) {
+        withExistingParent(name, mcLoc("item/generated"))
+                .texture("layer0", modLoc("item/" + name));
+    }
+
     @Override
     protected void registerModels() {
-        withExistingParent(Registration.AND_GATE_ITEM.get().getRegistryName().getPath(), modLoc("block/and_gate"));
-        withExistingParent(Registration.EXCLUSIVE_OR_GATE_ITEM.get().getRegistryName().getPath(), modLoc("block/xor_gate"));
-        withExistingParent(Registration.NOT_GATE_ITEM.get().getRegistryName().getPath(), modLoc("block/not_gate"));
+        registerGateModel("and_gate");
+        registerGateModel("not_gate");
+        registerGateModel("xor_gate");
+//        registerGateModel(Registration.AND_GATE_ITEM.get());
+//        registerGateModel(Registration.EXCLUSIVE_OR_GATE_ITEM.get());
+//        registerGateModel(Registration.NOT_GATE_ITEM.get());
+//        withExistingParent(Registration.AND_GATE_ITEM.get().getRegistryName().getPath(), modLoc("block/and_gate")).texture("layer0");
+//        withExistingParent(Registration.EXCLUSIVE_OR_GATE_ITEM.get().getRegistryName().getPath(), modLoc("block/xor_gate"));
+//        withExistingParent(Registration.NOT_GATE_ITEM.get().getRegistryName().getPath(), modLoc("block/not_gate"));
     }
 }
